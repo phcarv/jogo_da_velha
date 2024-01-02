f2e95e7e95ffafc7b803dac7d093cdd6877c62e1
phcarv <p.hcarvalho@outlook.com>
2024-01-02 12:28:01 -0300

    a really bad work around

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,60 @@
 public class Board {
 
-    private String[][] board;
-
     public Board(){
         this.board = new String[][]{
                 {" ", " ", " "},
                 {" ", " ", " "},
                 {" ", " ", " "}};
+
+        this.p11 = this.board[0][0];
+        this.p12 = this.board[0][1];
+        this.p13 = this.board[0][2];
+        this.p21 = this.board[1][0];
+        this.p22 = this.board[1][1];
+        this.p23 = this.board[1][2];
+        this.p31 = this.board[2][0];
+        this.p32 = this.board[2][1];
+        this.p33 = this.board[2][2];
     }
+    private String[][] board;
 
+    private String p11;
+    private String p12;
+    private String p13;
+    private String p21;
+    private String p22;
+    private String p23;
+    private String p31;
+    private String p32;
+    private String p33;
 
+    private Boolean checkWin(){
+        if(!this.p11.equals(" ") & this.p11.equals(this.p12) & this.p12.equals(this.p13)){
+            return true;
+        }
+        else if(!this.p21.equals(" ") & this.p21.equals(this.p22) & this.p22.equals(this.p23)){
+            return true;
+        }
+        else if(!this.p11.equals(" ") & this.p31.equals(this.p32) & this.p32.equals(this.p33)){
+            return true;
+        }
+        else if(!this.p11.equals(" ") & this.p11.equals(this.p21) & this.p21.equals(this.p31)){
+            return true;
+        }
+        else if(!this.p12.equals(" ") & this.p12.equals(this.p22) & this.p22.equals(this.p32)){
+            return true;
+        }
+        else if(!this.p13.equals(" ") & this.p13.equals(this.p23) & this.p23.equals(this.p33)){
+            return true;
+        }
+        else if(!this.p11.equals(" ") & this.p11.equals(this.p22) & this.p22.equals(this.p33)){
+            return true;
+        }
+        else if(!this.p13.equals(" ") & this.p13.equals(this.p22) & this.p22.equals(this.p31)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
--- src/Velha.java
+++ src/Velha.java
@@ -20,16 +20,38 @@ public class Velha {
                                3. A primeira pessoa a marcar 3 simbolos iguais
                                   em uma linha reta vence.""");
 
+        this.board = new String[][]{
+                {" ", " ", " "},
+                {" ", " ", " "},
+                {" ", " ", " "}};
+
+        this.p11 = this.board[0][0];
+        this.p12 = this.board[0][1];
+        this.p13 = this.board[0][2];
+        this.p21 = this.board[1][0];
+        this.p22 = this.board[1][1];
+        this.p23 = this.board[1][2];
+        this.p31 = this.board[2][0];
+        this.p32 = this.board[2][1];
+        this.p33 = this.board[2][2];
+
         while(!this.win){
             this.game();
         }
+
     }
 
-     private String[][] plays = {
-             {" ", " ", " "},
-             {" ", " ", " "},
-             {" ", " ", " "}
-    };
+    private String[][] board;
+
+    private String p11;
+    private String p12;
+    private String p13;
+    private String p21;
+    private String p22;
+    private String p23;
+    private String p31;
+    private String p32;
+    private String p33;
 
 //    private void checkWin(){
 //        for(int i = 0; i < 3; i++){
@@ -62,7 +84,7 @@ public class Velha {
                    %s  |  %s  |  %s
                       |     |
                 
-                """.formatted(this.flattenMatrix(this.plays));
+                """.formatted(this.flattenMatrix(this.board));
         System.out.println(grid);
     }
 
@@ -103,7 +125,7 @@ public class Velha {
 
 
     private Boolean playValid() {
-        if(this.plays[this.currentPlay[0]][this.currentPlay[1]].equals(" ")){
+        if(this.board[this.currentPlay[0]][this.currentPlay[1]].equals(" ")){
             this.gridFormatter();
             return true;
         }
@@ -114,12 +136,42 @@ public class Velha {
     }
 
     private void gridFormatter() {
-        plays[currentPlay[0]][currentPlay[1]] = (currentPlayer == 1) ? "X" : "O";
+        board[currentPlay[0]][currentPlay[1]] = (currentPlayer == 1) ? "X" : "O";
     }
 
 
     private void playerChange() {
         this.currentPlayer ^= 1;
+    }
+
+    private Boolean checkWin(){
+        if(!this.p11.equals(" ") & this.p11.equals(this.p12) & this.p12.equals(this.p13)){
+            return true;
+        }
+        else if(!this.p21.equals(" ") & this.p21.equals(this.p22) & this.p22.equals(this.p23)){
+            return true;
+        }
+        else if(!this.p11.equals(" ") & this.p31.equals(this.p32) & this.p32.equals(this.p33)){
+            return true;
+        }
+        else if(!this.p11.equals(" ") & this.p11.equals(this.p21) & this.p21.equals(this.p31)){
+            return true;
+        }
+        else if(!this.p12.equals(" ") & this.p12.equals(this.p22) & this.p22.equals(this.p32)){
+            return true;
+        }
+        else if(!this.p13.equals(" ") & this.p13.equals(this.p23) & this.p23.equals(this.p33)){
+            return true;
+        }
+        else if(!this.p11.equals(" ") & this.p11.equals(this.p22) & this.p22.equals(this.p33)){
+            return true;
+        }
+        else if(!this.p13.equals(" ") & this.p13.equals(this.p22) & this.p22.equals(this.p31)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public void game(){
@@ -130,6 +182,7 @@ public class Velha {
             this.currentPlay = this.playFormatter(this.typedPlay);
             isValid = this.playValid();
         }
+        this.win = this.checkWin();
         this.playerChange();
     }
 }
